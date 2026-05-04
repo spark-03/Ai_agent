927f35670f77203be7c87bce28a551085f98ec09
spark-03 <123577615+spark-03@users.noreply.github.com>
2026-05-04 23:12:02 +0530

    Update orchestratorr.py

--- orchestratorr.py
+++ orchestratorr.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Configure Gemini
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def call_gemini(prompt):
     try:
